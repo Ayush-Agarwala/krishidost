760c41d680c5a207bb29abd21080e9234e1c1561
risgh <rishavghatak@gmail.com>
2024-09-05 09:49:32 +0530

    lang added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,71 @@
-/* eslint-disable no-unused-vars */
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
-import Card from './Card'
+import Card from './Card';
+
+const translations = {
+  en: {
+    title: "Krishi Dost",
+    description: "Friend for every Farmer",
+    ourservices:"Our Services",
+    cattleService: "Cattle Service",
+    cropService: "Crop Service",
+    cdisc:"Click here for cattle healthcare",
+    cropdis:"Click here for crop service"
+  },
+  hi: {
+    title: "कृषि दोस्त",
+    description: "हर किसान के लिए मित्र",
+    ourservices:"हमारी सेवाएँ",
+    cattleService: "पशु सेवा",
+    cropService: "फसल सेवा",
+    cdisc:"मवेशियों के स्वास्थ्य सेवा के लिए यहां क्लिक करें",
+    cropdis:"फसल सेवा के लिए यहां क्लिक करें",
+  },
+};
 
 const Home = () => {
+  const [language, setLanguage] = useState('en'); // default language is English
+
+  const changeLanguage = (lang) => {
+    setLanguage(lang);
+  };
+
   return (
     <div>
       {/* Hero Section */}
       <div className="home">
         <div className="overlay">
-          <h1>Krishi Dost</h1>
-          <p>Friend for every Farmer</p>
+          <h1>{translations[language].title}</h1>
+          <p>{translations[language].description}</p>
         </div>
       </div>
 
       <div id="services" className="services-header">
-        <h2>Our Services</h2>
+        <h2>{translations[language].ourservices}</h2>
       </div>
+
       {/* Card Section */}
       <div className="card-container">
-        
         <Card
           image="./Cattle.jpg"
-          title="Cattle Service"
-          description="Click here for cattle helthcare"
+          title={translations[language].cattleService}
+          description={translations[language].cdisc}
           linkToo="/cattle"
         />
         <Card
           image="./Crop1.jpg"
-          title="Crop Service"
-          description="Click here for crop service"
+          title={translations[language].cropService}
+          description={translations[language].cropdis}
           linkToo="/crop"
         />
+      </div>
+      {/* Language Switch Buttons */}
+      <div class="lang">
+        <button class="hindi" onClick={() => changeLanguage('en')}>English</button>
+        <button class="hindi" onClick={() => changeLanguage('hi')}>हिंदी</button>
       </div>
     </div>
   );
 };
 
 export default Home;
-
