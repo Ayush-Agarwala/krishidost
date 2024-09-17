bf92d0526e575ed56924bd14f513f4c8ba5ddb99
AyushAgarwala <agarwala010703@gmail.com>
2024-09-17 09:05:08 +0530

    Protected Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
@@ -11,16 +12,33 @@ import Dashboard from './components/Dashboard';
 
 const App = () => {
   const [loggedInUsername, setLoggedInUsername] = useState(null);
- 
+  const ProtectedRoute = ({ children }) => {
+    return loggedInUsername ? children : <Navigate to="/auth" />;
+  };
+
   return (
     <Router>
       <Navbar loggedInUsername={loggedInUsername} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
-        <Route path="/cattle" element={<CattleDiagnosisChatbot/>} />
-        <Route path="/DiseaseInfo" element={<DiseaseInfoComponent/>} />
-        <Route path="/dash" element={<Dashboard/>} />
+        <Route
+          path="/cattle"
+          element={
+            <ProtectedRoute>
+              <CattleDiagnosisChatbot />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/DiseaseInfo"
+          element={
+            <ProtectedRoute>
+              <DiseaseInfoComponent />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/dash" element={<Dashboard />} />
         <Route path="/auth" element={<Auth setLoggedInUsername={setLoggedInUsername} />} />
       </Routes>
     </Router>
@@ -28,3 +46,4 @@ const App = () => {
 };
 
 export default App;
+
